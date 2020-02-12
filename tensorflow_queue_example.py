796d4f74857f1a2a8a56200656b2d12ffed0d4fc
Malvtrics <malvtrics6945@gmail.com>
2020-02-12 22:16:29 +0800

    Update tensorflow_queue_example.py

--- tensorflow_queue_example.py
+++ tensorflow_queue_example.py
@@ -13,3 +13,27 @@ with tf.Session() as sess:
     for _ in range(5):
         v,_ = sess.run([x,q_inc])
         print(v)
+
+##线程协同
+import numpy as np
+import threading
+import time
+
+def MyLoop(coord,worker_id):
+    while not coord.should_stop():
+        if np.random.rand() < 0.1:
+            print('stopping from id: {}'.format(worker_id))
+            coord.request_stop()
+        else:
+            print('working on id: {}'.format(worker_id))
+        time.sleep(1)
+
+coord = tf.train.Coordinator()
+threads = [threading.Thread(target=MyLoop,args=(coord,i)) for i in range(5)]
+for t in threads: t.start()
+coord.join(threads)
+
+#all the threads will print their own id 
+#when one thread call the request_stop function 
+#param should_stop will be set to true and other thread will be stopped
+
