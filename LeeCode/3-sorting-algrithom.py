62270bedc5f6978eafb2ae06ebddbac03333ec4d
Malvtrics <malvtrics6945@gmail.com>
2020-03-19 20:53:28 +0800

    Create 3-sorting-algrithom.py

---
+++ LeeCode/3-sorting-algrithom.py
@@ -0,0 +1,56 @@
+'''
+基本的排序算法： 冒泡 插入
+常见排序算法： 归并 快速 拓扑
+其他排序算法 桶排序、堆排序
+'''
+def bubblesort(arr):
+    for i in range(1,len(arr)):
+        for j in range(0, len(arr)-i):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+    return arr
+
+def insertionSort2(arr): 
+    for i in range(1, len(arr)): 
+        key = arr[i] 
+        j = i-1
+        while j >=0 and key < arr[j] : 
+                arr[j+1] = arr[j] 
+                j -= 1
+        arr[j+1] = key 
+    return arr      
+  
+def merge(arr, l, m, r): 
+    n1 = m - l + 1
+    n2 = r- m 
+    L = [0] * (n1)
+    R = [0] * (n2)
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+    i = 0
+    j = 0
+    k = l
+    while i < n1 and j < n2 : 
+        if L[i] <= R[j]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+def mergeSort(arr,l,r): 
+    if l < r:       
+        m = int((l+(r-1))/2)
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r) 
