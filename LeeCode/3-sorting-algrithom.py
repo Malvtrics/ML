42acfc9b4ae27d7570a540e7d9929a8fccd1e8ed
Malvtrics <malvtrics6945@gmail.com>
2020-03-19 21:37:09 +0800

    Update 3-sorting-algrithom.py

--- LeeCode/3-sorting-algrithom.py
+++ LeeCode/3-sorting-algrithom.py
@@ -1,6 +1,6 @@
 '''
-基本的排序算法： 冒泡 插入
-常见排序算法： 归并 快速 拓扑
+基本的排序算法：冒泡 插入
+常见排序算法：归并 快排 拓扑
 其他排序算法 桶排序、堆排序
 '''
 def bubblesort(arr):
@@ -54,3 +54,18 @@ def mergeSort(arr,l,r):
         mergeSort(arr, l, m) 
         mergeSort(arr, m+1, r) 
         merge(arr, l, m, r) 
+
+def partition(arr,low,high): 
+    i = (low-1) 
+    pivot = arr[high]     
+    for j in range(low , high): 
+        if arr[j] <= pivot: 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return (i+1) 
+def quickSort(arr,low,high): 
+    if low < high: 
+        pi = partition(arr,low,high) 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high) 
