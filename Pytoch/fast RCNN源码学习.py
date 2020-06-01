678bf2da18f45223831259dd6a4a21c5ec3b8683
Malvtrics <malvtrics6945@gmail.com>
2020-06-01 08:32:16 +0800

    Update fast RCNN源码学习.py

--- Pytoch/fast RCNN源码学习.py
+++ Pytoch/fast RCNN源码学习.py
@@ -20,19 +20,65 @@ class roibatchLoader(data.Dataset):
 ratio_list, ratio_index = rank_roidb_ratio(roidb)
 
 datasets.factory 中 
-for year in ['2007', '2012']:
-  for split in ['train', 'val', 'trainval', 'test']:
-    name = 'voc_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
-#__sets是一个字典，根据名字生成一个匿名函数
+for year in ['2014']:
+  for split in ['train', 'val', 'capval', 'valminuscapval', 'trainval']:
+    name = 'coco_{}_{}'.format(year, split)
+    __sets[name] = (lambda split=split, year=year: coco(split, year))
+#__sets是一个字典，根据名字生成一个匿名函数，有两个参数split和year，冒号后面表示要执行的函数
 
 def get_imdb(name):
   """Get an imdb (image database) by name."""
   if name not in __sets:
     raise KeyError('Unknown dataset: {}'.format(name))
-  return __sets[name]()
+  return __sets[name]() #coco('train','2014')
     
+#coco.py 继承了imdb
+from datasets.imdb import imdb
+class coco(imdb):
+  def __init__(self, image_set, year):
+    imdb.__init__(self, 'coco_' + year + '_' + image_set)
+    #后面就是如何加载coco数据集
+    self._data_path = osp.join(cfg.DATA_DIR, 'coco')
+    #在model/utils/config.py中可以看到DATA_DIR的定义
+    # Root directory of project
+    #__C.ROOT_DIR = osp.abspath(osp.join(osp.dirname(__file__), '..', '..', '..'))
+    #可以看出当时作者是把coco数据放在工程目录中训练的
+    self._COCO = COCO(self._get_ann_file()) #这一步用了coco自己的API，里面是自己的函数，拿到标注json文件路径
     
+  def _get_ann_file(self):
+      prefix = 'instances' if self._image_set.find('test') == -1 \ #太长的代码为了增强可读性，可以打斜杠到下一行去写
+      else 'image_info'
+      return osp.join(self._data_path, 'annotations',
+                  prefix + '_' + self._image_set + self._year + '.json')
+    # COCO API
+    #from pycocotools.coco import COCO
+    #下面的链接是pycocotools API的github地址，学习一下构造函数
+    #https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocotools/coco.py
+    
+    class COCO:
+      def __init__(self, annotation_file=None):
+          self.dataset,self.anns,self.cats,self.imgs = dict(),dict(),dict(),dict()  #dict的另外一种定义方式
+          self.imgToAnns, self.catToImgs = defaultdict(list), defaultdict(list)
+          #这里我们看到用了defaultdict为啥呢？？很有趣 defaultdict是dict的一个子类，重写了__missing__方法和增加了default_factory属性
+          #defaultfactory可以是list（可以用append），看可以是int（可以用来计数）等等比dict简单快速，复习的话可以看下面链接
+          #https://docs.python.org/3/library/collections.html#collections.defaultdict
+          #顺便看了下dict 发现一个有趣的知识点字典的比较，后面注意一下
+          #An equality comparison between one dict.values() view and another will always return False. 
+          #This also applies when comparing dict.values() to itself:
+          if not annotation_file == None:
+              print('loading annotations into memory...')
+              tic = time.time()
+              dataset = json.load(open(annotation_file, 'r'))
+              #第一次看到了assert的用法，工程中的使用，应该很有用
+              assert type(dataset)==dict, 'annotation file format {} not supported'.format(type(dataset))
+              print('Done (t={:0.2f}s)'.format(time.time()- tic))  #如何打印程序的执行时间
+              self.dataset = dataset
+              self.createIndex()
+              
+我们回到coco.py 接本文46行
+  cats = self._COCO.loadCats(self._COCO.getCatIds())
+  self._classes = tuple(['__background__'] + [c['name'] for c in cats]) 
+  #加一个背景类 知识点：数组可以用加号直接扩展，这里用tuple 类型其实是一种强调，因为tuple元素不能修改，所以强调分类数目是极其确定的
     
     
     
